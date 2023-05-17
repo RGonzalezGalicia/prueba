9f90929fd34dcdc667a2a9bd10079a8cb2eec7f9
Ricardo Gonzalez <ricardo_rlzda@outlook.com>
2023-05-17 15:47:50 -0600

    Primer commit

---
+++ RMPrueba/RMPrueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RMPrueba
+//
+//  Created by Ricardo Gonzalez on 17/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var buscadorTF: UITextField!
+    @IBOutlet weak var collectionPersonajes: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
